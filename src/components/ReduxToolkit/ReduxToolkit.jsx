f71808fb85760f2e3c5854f0916aad70aea51e7d
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-17 22:12:04 +0400

    feat(ReduxToolkit): Added counterSlice reducer

--- src/components/ReduxToolkit/ReduxToolkit.jsx
+++ src/components/ReduxToolkit/ReduxToolkit.jsx
@@ -1,11 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addInputValue } from "../../redux-toolkit/inputSlice";
+import { increment, decrement } from "../../redux-toolkit/counterSlice";
 
 function ReduxToolkit() {
   const value = useSelector((state) => state.input.value);
+  const count = useSelector((state) => state.counts.count);
   const dispatch = useDispatch();
+
   const handleInput = (event) => {
     dispatch(addInputValue(event.target.value));
+  };
+  const handleIncrementBtn = () => {
+    dispatch(increment());
+  };
+  const handleDecrementBtn = () => {
+    dispatch(decrement());
   };
 
   return (
@@ -13,6 +22,12 @@ function ReduxToolkit() {
       <h1> Redux Toolkit </h1>
       <input type="text" onChange={handleInput} />
       <p>{value}</p>
+
+      <div>
+        <h4>{count}</h4>
+        <button onClick={handleIncrementBtn}>+</button>
+        <button onClick={handleDecrementBtn}>-</button>
+      </div>
     </div>
   );
 }
--- src/redux-toolkit/storeTk.js
+++ src/redux-toolkit/storeTk.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import inputSlice from "../redux-toolkit/inputSlice";
+import counterSlice from "../redux-toolkit/counterSlice";
 
 export const storeTk = configureStore({
   reducer: {
     input: inputSlice,
+    counts: counterSlice,
   },
 });
